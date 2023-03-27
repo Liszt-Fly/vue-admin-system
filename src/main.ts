26763b42a1026f8e13eda8fde712da940a3aa9b7
Liszt-Fly <1904231450@qq.com>
2023-03-28 07:57:07 +0800

    🎉 init: init router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router, { setupRouter } from './router'
 
-createApp(App).mount('#app')
+async function bootstrap() {
+	const app = createApp(App)
+	setupRouter(app)
+	//等路由挂载好以后,再进行挂载
+	await router.isReady()
+	app.mount('#app')
+}
+bootstrap()
+
